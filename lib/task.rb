a0fa82d6adad61dc236235f14ff7fc6a78db79ef
Joey Sandine & Tyler Brown <student@epicodus.com>
2015-01-20 14:29:20 -0800

    Initial commit... yeah we're done

---
+++ lib/task.rb
@@ -0,0 +1,29 @@
+class Task
+  attr_reader(:description, :due_date, :list_id)
+
+  define_method(:initialize) do |attributes|
+    @description = attributes.fetch(:description)
+    @due_date = attributes.fetch(:due_date)
+    @list_id = attributes.fetch(:list_id)
+  end
+
+  define_singleton_method(:all) do
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch("description")
+      due_date = task.fetch("due_date")
+      list_id = task.fetch("list_id").to_i() # The information comes out of the database as a string.
+      tasks.push(Task.new({:description => description, :due_date => due_date, :list_id => list_id}))
+    end
+    tasks
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO tasks (description, due_date, list_id) VALUES ('#{@description}', '#{@due_date}', #{@list_id})")
+  end
+
+  define_method(:==) do |another_task|
+    self.description().==(another_task.description()).&(self.due_date().==another_task.due_date()).&(self.list_id().==(another_task.list_id()))
+  end
+end
